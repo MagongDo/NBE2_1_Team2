42a24ef12ce0b211d77bf4f9be765561a1e2e488
Web도승우 <140596632+MagongDo@users.noreply.github.com>
2024-09-12 12:00:30 +0900

    Update MvcConfiguration.java

--- coffee_Project/src/main/java/com/example/gccoffee/configuration/MvcConfiguration.java
+++ coffee_Project/src/main/java/com/example/gccoffee/configuration/MvcConfiguration.java
@@ -1,11 +1,22 @@
 package com.example.gccoffee.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
+   @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    objectMapper.registerModule(new JavaTimeModule());
+    return objectMapper;
+  }
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**").allowedOrigins("*");
